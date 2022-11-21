cfd1bd67564879f8e789823755a9cd898e103b64
Snipercadet <donibris@gmail.com>
2022-11-21 17:47:32 +0100

    modified swagger config

--- GoogleBooksAPI/Program.cs
+++ GoogleBooksAPI/Program.cs
@@ -22,11 +22,10 @@ builder.Services.AddSwaggerGen(q =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
